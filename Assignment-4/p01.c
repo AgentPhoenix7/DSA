ad017879033b5b38ab03abce943091bb96fe047c
Anis Mandal <83820979+AgentPhoenix7@users.noreply.github.com>
2025-10-16 08:54:44 +0530

    Fix function name for node creation in stack implementation

--- Assignment-4/p01.c
+++ Assignment-4/p01.c
@@ -11,7 +11,7 @@ typedef struct Stack {
 } Stack;
 
 // Function to create a new node
-Node* createLinkedList(int value) {
+Node* createNode(int value) {
     Node* newNode = (Node*)malloc(sizeof(Node));
     if (!newNode) {
         printf("Memory allocation failed\n");
@@ -29,7 +29,7 @@ void initStack(Stack* stack) {
 
 // Push operation
 void push(Stack* stack, int value) {
-    Node* newNode = createLinkedList(value);
+    Node* newNode = createNode(value);
     newNode->next = stack->top;
     stack->top = newNode;
 }
